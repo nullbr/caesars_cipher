8c9c208e1d4d356511b6632776e022d6b20a3f3a
nullbr <bmarianoleite3@gmail.com>
2022-06-09 09:58:07 -0300

    chore: test wrap upcase and wrap downcase

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -6,8 +6,12 @@ RSpec.describe CaesarCipher do
       expect(subject.caesar_cipher('Hello', 5)).to eq('Mjqqt')
     end
 
+    it 'wraps upcase letter around the alphabet' do
+      expect(subject.wrap_upcase(91)).to eq(65)
+    end
+
     it 'wraps the letter around the alphabet' do
-      expect(subject.caesar_cipher('w', 5)).to eq('b')
+      expect(subject.wrap_downcase(123)).to eq(97)
     end
   end
 end
